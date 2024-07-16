f0d7446986949be254190546b816d79fc777530b
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2024-07-16 19:10:40 +0800

    update: disable cross-site requests

--- middlewares/sessions.js
+++ middlewares/sessions.js
@@ -21,10 +21,14 @@ module.exports = session({
       secure: true, // if true: only transmit cookie over https, in prod, always activate this
       httpOnly: true, // if true: prevents client side JS from reading the cookie
       maxAge: 1000 * 60 * 30, // session max age in milliseconds
+
       // explicitly set cookie to lax
       // to make sure that all cookies accept it
       // you should never use none anyway
-      sameSite: 'lax',
+      // sameSite: 'lax',
+
+      // disable cross-site requests
+      sameSite: 'strict',
       // session expires after 5 minutes
       expires: 1000 * 60 * 5,
     },
